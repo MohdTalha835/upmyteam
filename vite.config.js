0de31e042dd35a88b63e77891c887d6849436328
Talha Shaikh <talhaytpgmail.com>
2025-09-27 15:55:41 +0530

    fix(vite): fontawsome import issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()         ],
+  optimizeDeps: {
+    include: ["@fortawesome/fontawesome-free/css/all.min.css"],
+  },
+});
